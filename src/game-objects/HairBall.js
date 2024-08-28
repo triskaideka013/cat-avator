d920b0fcc1f0af1bcf391067a49687195bd331ba
Chris(topher) Stadler <cstadler@larsontexts.com>
2024-08-28 09:46:45 -0400

    creates util/convert.js with degreesToRadians, degreesToVector2, radiansToVector2 exports; sets module type to NodeNext in jsconfig; annotates convert and HairBall files with jsdoc comments

--- src/game-objects/HairBall.js
+++ src/game-objects/HairBall.js
@@ -1,11 +1,17 @@
-// TODO:  add jsdoc, refer to github.com/KilledByAPixel/LittleJS src code for examples
+import { EngineObject, vec2, Vector2 } from "littlejsengine";
+import { degreesToVector2 } from "../util/convert";
+
 class HairBall extends EngineObject
 {
-  // TODO:  accept dir[ection] param so we can aim the hairball
-  constructor(pos)
+  /**
+   * 
+   * @param {Vector2} pos 
+   * @param {number} angle
+   */
+  constructor(pos, angle)
   {
     super(pos); // set position
-
-    this.velocity = vec2(-.25, 0); // set motion
+    
+    this.velocity = (!!angle) ? degreesToVector2(angle) : vec2(-.25, 0); // set motion to
   }
 }
---
+++ src/util/convert.js
@@ -0,0 +1,36 @@
+import { Vector2 } from "littlejsengine";
+
+/**
+ * 
+ * @param {number} deg 
+ * @returns
+ */
+export function degreesToRadians(deg) {
+  return deg * (Math.PI/180)
+}
+
+/**
+ * 
+ * @param {number} rad 
+ * @param {boolean} clockwise 
+ * @returns 
+ */
+export function radiansToVector2(rad, clockwise=false) {
+    
+    if (!!clockwise) {
+        // clockwise [0, -1] going to [1, 0]
+        return new Vector2(Math.sin(rad), -Math.cos(rad)); 
+    }
+
+    // conter-clockwise [1, 0] going to [0, 1]
+    return new Vector2(Math.cos(rad), Math.sin(rad));
+}
+
+/**
+ * 
+ * @param {number} deg 
+ * @returns 
+ */
+export function degreesToVector2(deg) {
+    return radiansToVector2( degreesToRadians(deg) ); 
+}
