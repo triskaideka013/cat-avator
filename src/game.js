f122d37c8360c3062b5bcbd7da5d9bc40fc23ab9
Mr. Bickelsworth <abickel@larsontexts.com>
2024-09-03 10:02:28 -0400

    update game.js to select level when elevator button pressed

--- src/game.js
+++ src/game.js
@@ -1,46 +1,105 @@
-
-//import { drawTextScreen, engineInit, mainCanvasSize, mouseWasPressed, mousePos, Sound } from "../node_modules/littlejsengine/dist/littlejs.esm.js";
-//import { HairBall } from "./game-objects/HairBall.js";
-
-'use strict';
+"use strict";
+glEnable = false;
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameInit()
-{
-    // called once after the engine starts up
-    // setup the game
+// State Management
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @type {GameStageManager}
+ */
+const stateManager = new GameStateManager();
+
+///////////////////////////////////////////////////////////////////////////////
+// Initial Game Setup
+///////////////////////////////////////////////////////////////////////////////
+
+function gameInit() {
+  console.log("initializing intro stage");
+  stateManager.initIntro();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameUpdate()
-{
-    // called every frame at 60 frames per second
-    // handle input and update the game state
-    if (mouseWasPressed(0)) {
-        new HairBall(mousePos); // no angle specified, defaults to 45 degrees
-    }
+// Main Update Loop
+///////////////////////////////////////////////////////////////////////////////
+
+function gameUpdate() {
+  // update the active stage
+  stateManager.getCurrentStage().gameUpdate();
+
+  // don't interrupt the current level
+  if (stateManager.currentStageIsActive()) return;
+
+  //stage is complete, decide what to do next
+  console.log("stage complete. resolving next action");
+  resolveNextStage();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameUpdatePost()
-{
-    // called after physics and objects are updated
-    // setup camera and prepare for render
+function gameUpdatePost() {
+  // called after physics and objects are updated
+  // setup camera and prepare for render
+  var currentStage = stateManager.getCurrentStage();
+
+  if (currentStage.gameUpdatePost) {
+    currentStage.gameUpdatePost();
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameRender()
-{
-    // called before objects are rendered
-    // draw any background effects that appear behind objects
+function gameRender() {
+  // called before objects are rendered
+  // draw any background effects that appear behind objects
+  var currentStage = stateManager.getCurrentStage();
+
+  if (currentStage.gameRender) {
+    currentStage.gameRender();
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameRenderPost()
-{
-    // called after objects are rendered
-    // draw effects or hud that appear above all objects
-    drawTextScreen('Hello World!', mainCanvasSize.scale(.5), 80);
+function gameRenderPost() {
+  // called after objects are rendered
+  // draw effects or hud that appear above all objects
+  var currentStage = stateManager.getCurrentStage();
+
+  if (currentStage.gameRenderPost) {
+    currentStage.gameRenderPost();
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// State Helpers
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Determine the next stage to be loaded
+ * and initialize it
+ */
+function resolveNextStage() {
+    // sanity check for game over
+  if (stateManager.isGameOver()) {
+    console.log("Y'all done died, son.");
+    return;
+  }
+
+  // leaving the intro?
+  if(stateManager.isIntroStage())
+  {
+    // leave intro state
+    stateManager.completeIntro();
+    stateManager.goToElevator();
+    return;
+  }
+
+  // has selected level?
+  if(stateManager.isElevatorStage())
+  {
+    // create a new level
+    var platformer = new PlatformerStage();
+    //transition to level
+    stateManager.startNewLevel(platformer);
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
