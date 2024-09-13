1a30a775b5559289ea38c6141f7d61f875d9bd4e
Mr. Bickelsworth <abickel@larsontexts.com>
2024-09-13 03:26:24 -0400

    removes platform object factory classes

--- src/stages/platformer/factories/powerupFactory.js
+++
@@ -1,7 +0,0 @@
-class PowerupFactory {
-  static createPowerups(powerupList) {
-    return powerupList.map(powerup => {
-      return new PowerUp(vec2(powerup.x, powerup.y), vec2(1, 1), new Color(1, 1, 0));
-    });
-  }
-}
--- src/stages/platformer/platformerStage.js
+++ src/stages/platformer/platformerStage.js
@@ -20,15 +20,37 @@ class PlatformerStage extends StageBase {
     cameraScale = 4 * 12;
 
     // initialize platforms and powerups
-    this.platforms = PlatformFactory.createPlatforms(this.levelConfig.platforms);
-    this.powerups = PowerupFactory.createPowerups(this.levelConfig.powerups);
+    this.platforms = this.levelConfig.platforms.map(
+      (p) =>
+        new Platform(
+          2,
+          vec2(p.x, p.y),
+          vec2(p.width, p.height),
+          new Color(1, 0, 0)
+        )
+    );
+
+    this.powerups = this.levelConfig.powerups.map(p => new PowerUp(vec2(p.x, p.y), vec2(1, 1), new Color(1, 1, 0)));
 
     // initialize enemies
-    this.enemies = EnemyFactory.createEnemies(this.platforms, this.levelConfig.enemies, this.levelConfig.enemySpeeds);
+    this.enemies = this.levelConfig.enemies.map(
+      (e, i) =>
+        new NPCCat(
+          "rat",
+          this.platforms[e].pos,
+          vec2(2, 2),
+          new Color(1, 0, 0),
+          this.platforms[e],
+          this.levelConfig.enemySpeeds?.[i] ?? vec2(2 / 60, 0),
+          true
+        )
+    );
 
-    this.yarns = this.levelConfig.yarnballs.map(yarn => new HairBall(vec2(yarn.x, yarn.y), 0));
+    this.yarns = this.levelConfig.yarnballs.map(
+      (yarn) => new HairBall(vec2(yarn.x, yarn.y), 0)
+    );
 
-    window.addEventListener('enemy-destroyed', this.enemyCleanup.bind(this));
+    window.addEventListener("enemy-destroyed", this.enemyCleanup.bind(this));
 
     // find pos.y of the lowest platform in level
     this.minimumStageY = 0;
@@ -36,17 +58,14 @@ class PlatformerStage extends StageBase {
       if (p.pos.y < this.minimumStageY) this.minimumStageY = p.pos.y;
     });
     // initialize player
-    this.player = new Player(
-      vec2(10, 4),
-      {...this}
-    );
+    this.player = new Player(vec2(10, 4), { ...this });
 
     // center camera on player
     cameraPos = this.player.pos.add(vec2(10, 0));
   }
 
   enemyCleanup(event) {
-    this.enemies = this.enemies.filter(e => e !== event.detail);
+    this.enemies = this.enemies.filter((e) => e !== event.detail);
   }
 
   gameUpdate() {
@@ -59,8 +78,8 @@ class PlatformerStage extends StageBase {
     }
 
     if (mouseWasPressed(0) && this.powerupManager.getYarnBallCount() > 0) {
-        new HairBall(this.player.pos, pointsToAngle(this.player.pos, mousePos));
-        this.powerupManager.removeYarnBall();
+      new HairBall(this.player.pos, pointsToAngle(this.player.pos, mousePos));
+      this.powerupManager.removeYarnBall();
     }
 
     if (this.player.hasWon()) {
@@ -77,7 +96,11 @@ class PlatformerStage extends StageBase {
   gameRenderPost() {
     if (!this.state.isActive()) return;
 
-    drawTextScreen(`🧶(yarnballs): ${this.powerupManager.getYarnBallCount()}` , vec2(110, 30), 30);
+    drawTextScreen(
+      `🧶(yarnballs): ${this.powerupManager.getYarnBallCount()}`,
+      vec2(110, 30),
+      30
+    );
 
     drawTextScreen(
       `Score: ${this.player.getScore()}`,
@@ -96,15 +119,22 @@ class PlatformerStage extends StageBase {
       new Color(1, 1, 1)
     );
 
-    [this.platforms, this.powerups, this.enemies, [this.player]].forEach(lst => lst.forEach(el => el.render()));
+    [this.platforms, this.powerups, this.enemies, [this.player]].forEach(
+      (lst) => lst.forEach((el) => el.render())
+    );
 
     // center on player
     cameraPos = this.player.pos.add(vec2(10, 0));
   }
 
   // destroy game objects
-  teardown()
-  {
-    [[this.player], this.platforms, this.powerups, this.enemies, this.yarns].forEach(lst => lst.forEach(el => el.destroy()));
+  teardown() {
+    [
+      [this.player],
+      this.platforms,
+      this.powerups,
+      this.enemies,
+      this.yarns,
+    ].forEach((lst) => lst.forEach((el) => el.destroy()));
   }
 }
